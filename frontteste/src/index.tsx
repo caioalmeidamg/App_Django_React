8ab746fec3045acb636846b9da1e820c6cdb0ad9
Caio Santos <aceutoxzc@gmail.com>
2023-10-18 11:25:22 -0300

    alterações e estudos de react

--- frontteste/src/index.tsx
+++ frontteste/src/index.tsx
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Esse root é uma criação do react utilizada para renderizar a aplicação
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
