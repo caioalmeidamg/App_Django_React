cabf07ddc655e21178360b8fed2f566b6f1225f2
Caio Almeida Santos <aceutoxzc@gmail.com>
2023-10-18 19:53:35 -0300

    comentario adicionado

--- myapi/myapp/urls.py
+++ myapi/myapp/urls.py
@@ -3,6 +3,7 @@ from .views import ProdutoListCreateView
 
 #urls.py é o arquivo que detecta as rotas da aplicação
 
+#Esse arquivo para rotas serve como uma  espécie de handler da aplicação
 urlpatterns = [
     path('produtos/', ProdutoListCreateView.as_view(), name='produto-list-create'),
 ]
