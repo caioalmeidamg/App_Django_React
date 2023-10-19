efa6ea740b333c9aaf24ce309bf4c4299088a3b4
Caio Santos <aceutoxzc@gmail.com>
2023-10-19 11:36:04 -0300

    mudança de estrutura

--- frontteste/src/index.tsx
+++ frontteste/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
 //Esse root é uma criação do react utilizada para renderizar a aplicação
