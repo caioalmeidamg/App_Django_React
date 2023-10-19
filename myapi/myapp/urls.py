e0e69ec897398bc74bcaec30af3a370361c84562
Caio Almeida Santos <aceutoxzc@gmail.com>
2023-10-18 21:03:36 -0300

    método post implementado

--- myapi/myapp/urls.py
+++ myapi/myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProdutoListCreateView
+from .views import ProdutoListCreateView, criar_produto
 
 #urls.py é o arquivo que detecta as rotas da aplicação
 
 #Esse arquivo para rotas serve como uma  espécie de handler da aplicação
 urlpatterns = [
+    path('produtos/', criar_produto),
     path('produtos/', ProdutoListCreateView.as_view(), name='produto-list-create'),
 ]
--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Produto
 from .serializers import ProdutoSerializer
+
 
 class ProdutoListCreateView(generics.ListCreateAPIView):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
+
+#Esse pelo visto é um método post padrão para um produto sem um serializer bem trabalhado
+@api_view(['POST'])
+def criar_produto(request):
+    if request.method == 'POST':
+        serializer = ProdutoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
