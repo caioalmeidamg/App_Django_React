1b55433d1290839f31c117fb20eed05867542bf0
Caio Santos <aceutoxzc@gmail.com>
2023-10-19 10:41:31 -0300

    mudanças realizadas

--- myapi/myapp/urls.py
+++ myapi/myapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ProdutoListCreateView, criar_produto
+from .views import ProdutoListCreateView, resposta
 
 #urls.py é o arquivo que detecta as rotas da aplicação
 
 #Esse arquivo para rotas serve como uma  espécie de handler da aplicação
 urlpatterns = [
     #Esse path aqui serve como rota para um post de Produtos
-    path('produtos/', criar_produto),
+    path('produtos/', resposta.criar_produto),
+    #path('produto/<int:produto_id>/', views.obter_produto, name='obter_produto'),
     path('produtos/', ProdutoListCreateView.as_view(), name='produto-list-create'),
 ]
--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics,status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Produto
+from django.http import JsonResponse
 from .serializers import ProdutoSerializer
 
 #Essa classe aqui funciona como um retorno de uma view dos dados de uma Api
@@ -10,12 +11,19 @@ class ProdutoListCreateView(generics.ListCreateAPIView):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
 
-#Esse pelo visto é um método post padrão para um produto sem um serializer bem trabalhado
-@api_view(['POST'])
-def criar_produto(request):
-    if request.method == 'POST':
-        serializer = ProdutoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+#Essa vai ser a classe padrão de resposta http para o crud
+class resposta:
+        
+    #Esse pelo visto é um método post padrão para um produto sem um serializer bem trabalhado
+    @api_view(['POST'])
+    def criar_produto(request):
+        if request.method == 'POST':
+            #Essa função verifica se os dados passados para p serializers são validos para o banco de dados
+            serializer = ProdutoSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+            #caso não seja possivel essa função retorna os erros que os serializer encontrou/gerou
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+        
+   
