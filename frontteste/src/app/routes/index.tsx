eb99787e7650990e9e73db7329d38631c6693c79
Caio Santos <aceutoxzc@gmail.com>
2023-10-24 10:18:13 -0300

    arquivo de rotas setado

--- frontteste/src/app/routes/index.tsx
+++ frontteste/src/app/routes/index.tsx
@@ -1,1 +1,14 @@
-export {};
+import { BrowserRouter, Route, Routes as Switch, Navigate  } from "react-router-dom";
+
+
+export const Routes = () => {
+    return (
+      <BrowserRouter>
+      <Switch>
+        <Route path="/" element={<h1>Olá mundo</h1>} />
+        <Route path="/pagina-inicial" element={<h1>teste</h1>} />
+      </Switch>
+      </BrowserRouter>
+    );
+}
+
