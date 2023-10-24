609107ad2f13de55edd66c3a109d404b70fc9a59
Caio Almeida Santos <aceutoxzc@gmail.com>
2023-10-24 16:24:10 -0300

    teste

--- frontteste/src/app/shared/components/botao.js
+++ frontteste/src/app/shared/components/botao.js
@@ -1,8 +1,12 @@
-import { Route } from "react-router-dom"
+import { Link, Route } from "react-router-dom"
 
 export const BotaoPadrap = () => {
     
-    return(         
-            <a href="http://localhost:3000/pagina-inicial"> <button></button></a>        
+    return(  
+           <div>  
+           /* Essa tag link serve como a tag a de html, ou seja, consigo fazer linkagem entre paginas com ela */ 
+           <Link to="http://localhost:3000/pagina-inicial"><button>TESTE</button></Link>  
+            
+            </div> 
     )
 }
