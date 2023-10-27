180741c91a4291e9f5a0028684ae03a074b4e5fc
Caio Santos <aceutoxzc@gmail.com>
2023-10-27 11:39:43 -0300

    boicote.py

--- myapi/myapp/urls.py
+++ myapi/myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import fun
+from .views import  resposta
 
 #urls.py é o arquivo que detecta as rotas da aplicação
 
@@ -7,7 +7,8 @@ from .views import fun
 urlpatterns = [
      
     #Esse path aqui serve como rota para um post de Produtos
-    path('produtos/', fun),
+    path('produtos/', resposta.criar_produto),
+    path('produtos/', resposta.criar_produto),
     #path('produto/<int:produto_id>/', views.obter_produto, name='obter_produto'),
     
 ]
--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -7,31 +7,20 @@ from django.http import JsonResponse
 from .serializers import ProdutoSerializer
 from django.views.decorators.csrf import csrf_exempt
 
+class ProdutoListCreateView(generics.ListCreateAPIView):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
 
 
-    
-   #não conseguir criar um atributo estatico pro serializer
-@csrf_exempt
-def fun(request):
-    if request.method == 'POST':
-       return JsonResponse(criar_produto(request)) 
-    
-#Esse pelo visto é um método post padrão para um produto sem um serializer bem trabalhado
+class resposta:
 
-def criar_produto(request):
-    if request.method == 'POST':
-        #Essa função verifica se os dados passados para p serializers são validos para o banco de dados
-        serializer = ProdutoSerializer(data=request.body)
-        print(request.body)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        #caso não seja possivel essa função retorna os erros que os serializer encontrou/gerou
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
-    
-#Essa classe aqui funciona como um retorno de uma view dos dados de uma Api
-
-def listar_produtos():
-    return "ProdutoSerializer.get_fields()"
+    @api_view(['POST'])
+    def criar_produto(request):
+        if request.method == 'POST':
+            serializer = ProdutoSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)     
 
     
