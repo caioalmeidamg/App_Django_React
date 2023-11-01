9fc5a12f75099a359e1024bf62989e3bf8c96777
Caio Santos <aceutoxzc@gmail.com>
2023-11-01 11:33:52 -0300

    relação 1:N implementada

--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -63,8 +63,10 @@ class respostaEstoque(APIView):
 
     def post(self, request, id = 0):       
         serializer = EstoqueSerializer(data=request.data)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
             
     
     def delete(self, request, id=0):
