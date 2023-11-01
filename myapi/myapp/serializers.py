aa735c6e00b1e3146559927c116f38582872a5e6
Caio Santos <aceutoxzc@gmail.com>
2023-11-01 10:42:53 -0300

    tentando criar relação

--- myapi/myapp/serializers.py
+++ myapi/myapp/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import Produto
+from .models import Produto, Estoque
 
 #Essa classe de serializers faz um papel muito parecido ao de uma JPA
 class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Produto
         fields = '__all__'
+
+
+#Essa classe de serializers faz um papel muito parecido ao de uma JPA
+class EstoqueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estoque
+        fields = '__all__'        
--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics,status
 from rest_framework.response import Response
 from .models import Produto
+from .models import Estoque
 from rest_framework.views import APIView
-from .serializers import ProdutoSerializer
+from .serializers import ProdutoSerializer, EstoqueSerializer
 
 class ProdutoListCreateView(generics.ListCreateAPIView):
     queryset = Produto.objects.all()
@@ -10,7 +11,7 @@ class ProdutoListCreateView(generics.ListCreateAPIView):
 
 
 #Classe de resposta da view
-class resposta(APIView):
+class respostaProduto(APIView):
     
     #no get nos adicionamos um valor default do id == 0 para que possamos arrumar as rotas
     def get(self, request, id = 0):
@@ -40,11 +41,41 @@ class resposta(APIView):
             except Produto.DoesNotExist:
                 return Response("not ok")
         else:
-            # Lógica para lidar com a ausência do parâmetro "id" ou outro valor padrão
-            return Response("Erro: ID não fornecido")
+            produto = Produto.objects.all()
+            produto.delete()
+            return Response("deletados")
         
 
+#Classe de resposta da view
+class respostaEstoque(APIView):
+    
+    #no get nos adicionamos um valor default do id == 0 para que possamos arrumar as rotas
+    def get(self, request, id = 0):
+        if id != 0:    
+            estoques = Estoque.objects.filter(ID = id)
+            serializer = EstoqueSerializer(estoques, many=True)
+            return Response(serializer.data)
+            
+        elif id == 0: 
+            estoques = Estoque.objects.all()
+            serializer = EstoqueSerializer(estoques, many=True)
+            return Response(serializer.data)
 
-        
-        
-        
+    def post(self, request, id = 0):       
+        serializer = EstoqueSerializer(data=request.data)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+            
+    
+    def delete(self, request, id=0):
+        if id != 0:
+            try:
+                estoques = Estoque.objects.get(ID=id)
+                estoques.delete()
+                return Response("ok")
+            except Estoque.DoesNotExist:
+                return Response("not ok")
+        else:
+            estoques = Estoque.objects.all()
+            estoques.delete()
+            return Response("deletados")
