b9c4f7d60924dae729ebd939c6005614bd8ff024
Caio Santos <aceutoxzc@gmail.com>
2023-11-07 11:41:38 -0300

    save de header

--- frontteste/src/app/shared/components/Header.js
+++ frontteste/src/app/shared/components/Header.js
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 import styles from './Header.module.css';
+import Sidebar from "./sidebarComponents/Sidebar";
+
+
 
 export const Header = (props) => {
     return (
       <header>
   
           
-      
-        <h1 className={(styles.apresenta)}> Olá mundo </h1>  
+        <Sidebar nome="Olá mundo"/>
     
         <div className={(styles.centralizado)}>
-          <Link to="/produtos" > <Button variant="contained" className={(styles.botao)}>Produtos</Button> </Link>
+          <Link to="/" > <Button variant="contained" className={(styles.botao)}>Home</Button> </Link>
           
             
-              <Link to="/produtos" > <Button variant="contained" className={(styles.botao)}>Mudar pagina</Button> </Link>
+              <Link to="/produtos" > <Button variant="contained" className={(styles.botao)}>Produtos</Button> </Link>
               <a> <Button variant="contained" className={(styles.botao)}>Mudar pagina</Button> </a>
               <a> <Button variant="contained" className={(styles.botao)}>Mudar pagina</Button> </a>
               <a> <button style={{ borderRadius: "30px" }}>Sair</button> </a>
