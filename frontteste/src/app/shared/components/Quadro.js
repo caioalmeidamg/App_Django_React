bff5658c22362aeb703cfe54217592ff102179bf
Caio Almeida Santos <aceutoxzc@gmail.com>
2023-11-15 01:17:29 -0300

    react-select resolvido

--- frontteste/src/app/shared/components/Quadro.js
+++ frontteste/src/app/shared/components/Quadro.js
@@ -1,4 +1,39 @@
+
 import styles from "./Quadro.module.css"
+import Select, { components} from 'react-select';
+import { purple } from "@mui/material/colors";
+
+const msgStyles = {
+  color: 'purple',
+};
+
+
+const CustomNoOptionsMessage = () => {
+  return (
+    //Resolvivel
+    <Select
+      isClearable
+      noOptionsMessage={() => "ok"}
+      //styles={{ noOptionsMessage: (base) => ({  ...msgStyles }) }}
+      isSearchable
+      name="color"
+      options={[]}
+    />
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 export const Quadro = (props) => {
   return (
     
@@ -7,6 +42,7 @@ export const Quadro = (props) => {
            
           <div className={(styles.quadro)}>
           <h2> Opa, testando a propriedade {props.nome}</h2>
+          <CustomNoOptionsMessage />
           </div>
         
      
